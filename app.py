fb7b475486bf2a1258da447b150a1af02d91df03
dnau6tm <97821449+dnau6tm@users.noreply.github.com>
2024-01-15 11:37:35 +0700

    add slide report

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import time
 import streamlit as st
 import numpy as np
 import pickle
+import pandas as pd
 
 from model import SentimentAnalysisModelViSo
 
@@ -24,6 +25,11 @@ def main():
           encoded_data = tokenizer([text_input], padding=True, truncation=True, max_length=256, add_special_tokens = True, return_tensors="pt")
           outputs = model(encoded_data['input_ids'], encoded_data['attention_mask'])
           prob = torch.sigmoid(outputs).cpu().detach().numpy()
+          st.text(prob)
+
+          for k in range(len(prob)):
+               if max(prob[k]) < 0.5:
+                    prob[k] = np.where(prob[k]>max(prob[k])-1e-4, 1, 0)
 
           mapped_array = np.where(prob > 0.5, 1, 0)
           original_labels = mlb.inverse_transform(mapped_array)[0]
@@ -31,16 +37,24 @@ def main():
           st.text(original_labels)
 
 
-@st.cache(allow_output_mutation=True)  # hash_func
+@st.cache_resource  # hash_func
 def load_model():
      print("Loading model ...")
      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+     list_emoji = pd.read_csv('./newtokens.txt', header=None)
+
+
      # Load pre-trained PhoBERT model and tokenizer
      viso_model= AutoModel.from_pretrained('uitnlp/visobert')
      viso_tokenizer = AutoTokenizer.from_pretrained('uitnlp/visobert')
+
+     viso_tokenizer.add_tokens(list(list_emoji[0]))
+     viso_model.resize_token_embeddings(len(viso_tokenizer))
+
+
      absa_model = SentimentAnalysisModelViSo(viso_model, 12).to(device)
-     absa_model.load_state_dict(torch.load('./absa_model_withemoji_viso_2.pth', map_location=torch.device('cpu')))
+     absa_model.load_state_dict(torch.load('./absa_model_visobert.pth', map_location=torch.device('cpu')))
      return absa_model, viso_tokenizer
 
 
